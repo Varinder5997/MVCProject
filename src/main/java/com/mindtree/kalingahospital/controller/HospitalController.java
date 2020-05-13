9a99e134f8d899d86a7b32d72c0e1bc6458878cc
Varinder5997 <35367596+Varinder5997@users.noreply.github.com>
2020-05-13 17:15:11 +0530

    Update HospitalController.java

--- src/main/java/com/mindtree/kalingahospital/controller/HospitalController.java
+++ src/main/java/com/mindtree/kalingahospital/controller/HospitalController.java
@@ -2,14 +2,8 @@ package com.mindtree.kalingahospital.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 
 
